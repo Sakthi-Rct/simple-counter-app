2d5f1dc83fe344300926a0548c4139e0920910f5
sakthivel-happyfox <sakthivel@happyfox.com>
2021-01-30 18:12:50 +0530

    simple counter app complted

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/Counters";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
